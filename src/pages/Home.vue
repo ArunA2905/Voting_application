e35ea203fd1b58d16b0a37e3b1956566163a3190
ArunA2905 <viswa6919@gmail.com>
2023-06-11 23:44:11 +0530

    Post API Completed

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -20,51 +20,12 @@
   import axios from 'axios'
   import data from '../assets/data'
   
-
-  const getData = () => {
-    const Votingdata = localStorage.getItem('votingData')
-    if (Votingdata === null){
-      return data
-    } else {
-      const parsedData = JSON.parse(Votingdata)
-      return parsedData
-    } 
-  }
-
-  const getDbData = () => {
-    
-  }
-
-  const getVoteSts = () => {
-    const userId = $cookies.get('auth-user')
-    
-    const userStats = localStorage.getItem(`user${userId}`)
-    if (userStats === null){
-      return false 
-    } else {
-      const parsedData = JSON.parse(userStats)
-      return parsedData.voteStats
-    }
-  }
-
-  const getLastIndex = () => {
-    const userId = $cookies.get('auth-user')
-
-    const userStats = localStorage.getItem(`user${userId}`)
-    if (userStats === null){
-      return null 
-    } else {
-      const parsedData = JSON.parse(userStats)
-      return parsedData.indexStats
-    }
-  }
-  
   export default {
   data() {
     return {
-      items: getData(),
-      isVoted: getVoteSts(),
-      lastIndex: getLastIndex()
+      items: [],
+      isVoted: false,
+      lastIndex: null
     };
   },
   beforeMount() {
@@ -73,24 +34,35 @@
       window.location.href = '/login'
     }
   },
-  created() {
+  async created() {
     axios.get('http://localhost:3000/data',)
   .then(response => {
-    //this.items =  response.data
-    //console.log(response.data)
+    const data = response.data
+    const arrangedData = data.sort((a, b) => a.id - b.id);
+    this.items =  arrangedData
   })
   .catch(error => {
     console.error(error);
   });
 
-  axios.get('http://localhost:3000/user_stats',)
-  .then(response => {
-    //this.items =  response.data
-    //console.log(response.data)
-  })
-  .catch(error => {
+  let userStatus;
+
+  try {
+    const response = await axios.get('http://localhost:3000/user_stats');
+    userStatus = response.data;
+  } catch (error) {
     console.error(error);
-  });
+  }
+
+  const userid = this.$cookies.get('auth-user')
+
+  const isUserStatId = userStatus.filter(each => each.userId === parseInt(userid))
+  //console.log(isUserStatId)
+
+  if (isUserStatId.length !== 0) {
+    this.isVoted = userStatus[0].voteStatus
+    this.lastIndex = userStatus[0].lastVote
+  }
  },
   methods: {
     vote(index) {
@@ -111,33 +83,62 @@
         this.lastIndex = index
       }
     },
-    save() {
-      const votingData = this.items
-      const jsonData = JSON.stringify(votingData)
-      localStorage.setItem('votingData', jsonData)
-
-      const userid = this.$cookies.get('auth-user')
-     // const userStats = {userId: userid, voteStats: this.isVoted, indexStats: this.lastIndex}
-     // const userStats = {userId: userid, voteStatus: this.isVoted, lastVote: this.lastIndex}
-     const userStats = {userId: 1, voteStatus: true, lastVote: 1}
-      const userstatsJson = JSON.stringify(userStats)
-      localStorage.setItem(`user${userid}`, userstatsJson)
-
-      axios.post('http://localhost:3000/user_stats', {
-        user_stat: {
-          ...userStats
-        }
-      }, {
-        headers: {
-          'Content-Type': 'application/json'
-        }
-      }).then(response => {
-          console.log(userstatsJson)
-          console.log(response);
+    async save() {
+      this.items.forEach((item,i) => {
+      const data = { vote: item.vote };
+    
+      axios.put(`http://localhost:3000/data/${i+1}`, data)
+        .then(response => {
+          //console.log(response.data);
         })
         .catch(error => {
           console.error(error);
         });
+      });
+
+      const userid = this.$cookies.get('auth-user')
+      const userStats = {userId: userid, voteStatus: this.isVoted, lastVote: this.lastIndex}
+        
+      let userStatus;
+
+      try {
+        const response = await axios.get('http://localhost:3000/user_stats');
+        userStatus = response.data;
+      } catch (error) {
+        console.error(error);
+      }
+
+      const userPresent = userStatus.find(eachItem => eachItem.userId === parseInt(userid))
+      
+      if (userPresent === undefined){
+        axios.post('http://localhost:3000/user_stats', {
+        user_stat: {
+          ...userStats
+        }
+        }, {
+          headers: {
+            'Content-Type': 'application/json'
+          }
+        }).then(response => {
+            //console.log(response);
+          })
+          .catch(error => {
+            console.error(error);
+          });
+      } else {
+        const data = {
+          voteStatus: this.isVoted,
+          lastVote: this.lastIndex,
+        };
+
+        axios.put(`http://localhost:3000/user_stats/${userid}`, data)
+          .then(response => {
+            //console.log(response.data);
+          })
+          .catch(error => {
+            console.error(error);
+          });
+      }
     },
     reset() {
       localStorage.clear();
@@ -152,5 +153,4 @@
 </script>
 
 <style scoped src="../assets/css/home.css">
-/* @import '../assets/css/home.css'; */
 </style>
